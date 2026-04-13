333873b300bc6faa56ea0340b2802d7c4f0c5f39
Paulo Ferreira <pauloflausino@gmail.com>
2026-04-13 19:33:02 -0300

    config correcao

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/uncaodoleao',
+  assetPrefix: '/uncaodoleao',
   typescript: {
     ignoreBuildErrors: true,
   },
